5a26a628011edb7d9fecf779f26bb8796999447b
Gabriel <gabrielsilvalucio@gmail.com>
2023-03-20 19:39:35 +0000

    Searchbar JSX + SCSS

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 
+import videos from "../../Constants/videos";
+
+import {AiOutlineSearch} from "react-icons/ai";
+
 import './Header.css';
 
 const Header = () => {
     return (
         <header className="app__header">
             <section className="app__header-container">
-                <div className="app__header-search">
-                    
+                
+                <div className="app__header-vid">
+                   <video 
+                    type="video/mp4"
+                    src={videos.backgroundvid} 
+                    autoPlay
+                    loop 
+                    muted
+                    />
+                </div>
+                
+                <div className="app__header-searchbar">
+                    <form action="" className="app__sform">
+                        <input 
+                            className="app__input"
+                            type="text"
+                            placeholder="Procura por um champion..."
+                            required
+                        />
+                         
+                         <button>
+                            <AiOutlineSearch className="search-icon" />
+                        </button>
+                    </form>
+
                 </div>
             </section>
 
---
+++ src/Constants/videos.js
@@ -0,0 +1,5 @@
+import backgroundvid from './../Assets/backgroundvid.mp4'
+
+export default {
+    backgroundvid
+};
