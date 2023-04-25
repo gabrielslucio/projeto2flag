233d8639fae8ee76d52e9ba719dd16c084503519
Gabriel <gabrielsilvalucio@gmail.com>
2023-04-25 15:04:59 +0100

    ChampionPage JSX + SCSS

--- src/Pages/ChampionPage/ChampionPage.jsx
+++ src/Pages/ChampionPage/ChampionPage.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router";
 import champion from "../../Constants/champion.json";
 import championImages from "../../Constants/championImages";
 
+import {BsFillArrowUpCircleFill, BsFillArrowDownCircleFill, BsEyeFill, BsChatDotsFill} from "react-icons/bs"
+
 import './ChampionPage.css';
 
 const ChampionPage = () => {
@@ -15,11 +17,38 @@ const ChampionPage = () => {
     //encontra o index da imagem do campeão
     const index = Object.keys(champion.data).findIndex(key => key.toLowerCase() === championId);
 
+    // obtém o nome do campeão
+    const championName = champion.data[Object.keys(champion.data)[index]].name;
+
+
     return (
-        <section className="app__champion">
+        
+        <section className="app__champion-page">
+            <div className="app__champion-container">
             <div className="app__champion-banner">
-                <img src={championImages[index]} alt={championId} />
+               { /* <div className="app__champion-info">
+                        <h1>{championName} guia por <span>Gabriel</span></h1>
+                        <p>UPDATED EM ABRIL 25, 2023</p>
+                        <BsFillArrowUpCircleFill />
+                        <BsFillArrowDownCircleFill />
+
+                        <div className="app__champion-views">
+                            <BsEyeFill />
+                            <p>30,000 visualizações</p>
+                        </div>
+
+                        <div className="app__champion-comments">
+                            <BsChatDotsFill />
+                            <p>6 comentários</p>
+                        </div>
+                </div> */}
+
+                <div className="app__champion-img">
+                    <img src={championImages[index]} alt={championId} />
+                </div>
             </div>
+            </div>
+            
         </section>
     );
 }
