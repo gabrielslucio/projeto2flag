bdf6a696bd7110777b43fbbe03cc5e44a9db30ca
Gabriel <gabrielsilvalucio@gmail.com>
2023-05-05 13:16:03 +0100

    MyAccount JSX + SCSS + Lógica Modal

--- src/Pages/MyAccount/MyAccount.jsx
+++ src/Pages/MyAccount/MyAccount.jsx
@@ -1,24 +1,120 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {AiOutlineClose} from "react-icons/ai";
 
 import './MyAccount.css';
+
+import championImages from "../../Constants/championImages";
 import images from "../../Constants/images";
 
+/* 
+DOCUMENTAÇÃO DE CÓDIGO - CRIAÇÃO DE MODAL 
+1) Criar uma state de controlo;
+2) Criar boolean com um handleChooseImageClick function para mostrar conteúdo;
+3) Criar boolean para fechar o modal;
+4) Adicionar o onClick ao butão.
+*/
+
+/*
+DOCUMENTAÇÃO DE CÓDIGO - IMAGEM SELECIONADA
+1) Criar uma state de controlo;
+2) Criar uma função de controlo;
+3) Adicionar conditional render.
+
+*/ 
+
 const MyAccount = () => {
+
+    const [showModal, setShowModal] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(localStorage.getItem('selectedImage') || null);
+
+    const currentUser = JSON.parse(localStorage.getItem('user'));
+
+    const handleChooseImageClick = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    const handleImageSelect = (image) => {
+        setSelectedImage(image);
+    };
+
+    const handleChooseNewImage = () => {
+        if (selectedImage) {
+            localStorage.setItem("selectedImage", selectedImage)
+        }
+        setShowModal(false);
+    }
+
+    useEffect(() => {
+        setSelectedImage(localStorage.getItem('selectedImage'));
+    }, []);
+
     return (
         <section className="app__my-account">
             <div className="app__my-account-container">
                 <div className="app__profile-pic">
-                    <h1>Username</h1>
-                    <img src={images.aatrox} alt="profile"/>
-                    <button>Escolher nova imagem</button>
+                    <h1>{currentUser.username}</h1>
+                    {selectedImage ? (
+                        <img 
+                            src={selectedImage}
+                            alt="profile"
+                            className="selected-image"
+                        />
+                    ) : (
+                        <img src={images.aatrox} alt="profile"/>
+                    )}
+                    
+                    <button onClick={handleChooseImageClick}>Escolher nova imagem</button>
                 </div>
 
                 <div className="app__profile-user">
                     <h2>Os meus guias</h2>
                     <button>Ver guias</button>
-                </div>
-                
+                </div>                
             </div>
+
+            {showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                       
+
+                        <div className="modal-header">
+                            <div className="ghost">
+                                
+                            </div> 
+
+                            <h3>Escolha uma nova <span>imagem</span></h3>
+
+                            <div className="modal-btn-close">
+                                <button onClick={handleCloseModal} type="button"><AiOutlineClose /></button>
+                            </div>
+                        </div>
+                       
+
+                        <div className="modal-container">
+                            <div className="modal-images">
+                                {championImages.map((image, index) => (
+                                    <img 
+                                        key={index} 
+                                        src={image} 
+                                        alt="picture" 
+                                        className={selectedImage === image ? "selected" : ""}
+                                        onClick={() => handleImageSelect(image)}
+                                    />
+                                ))}
+                            </div>
+                        </div>                        
+
+                            <div className="modal-btn-choose">
+                                <button onClick={handleChooseNewImage}>Escolher nova imagem</button>
+                            </div>
+                    </div>
+                </div>
+            )}
+
         </section>
     );
 }
