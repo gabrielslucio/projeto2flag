4ec059925125051297b8693239eda95c66d3bd69
Gabriel <gabrielsilvalucio@gmail.com>
2023-05-14 21:37:44 +0100

    EditComments JSX + SCSS

--- src/Pages/EditComments/EditComments.jsx
+++ src/Pages/EditComments/EditComments.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 
 import './EditComments.css';
 
 const EditComments = () => {
+
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+    const [selectedImage, setSelectedImage] = useState(localStorage.getItem('selectedImage') || null);
+
+
     return (
         <section className="app__edit-comments">
             <div className="app__edit-comments-container">
@@ -27,10 +32,146 @@ const EditComments = () => {
                 </div>           
 
                 <div className="app__cms-menu">
-                </div>   
+                        <div className="app__cms-c-header">
+                            <h2>Todos os comentários de: <span>{currentUser.username}</span></h2>
+                        </div>
 
+                        <div className="app__cms-comments-list">
+                          
+                            <div className="app__cms-c">
+                                <div className="app__user-pic">
+                                    <img src={selectedImage} alt="profile" />
+                                </div>
 
-            </div>
+                                <div className="app__user-name">
+                                    <span>{currentUser.username}</span>
+                                </div>
+
+                                <button>Apagar</button>
+
+                                <div className="app__guide-name">
+                                    <p>Aatrox Top - "O melhor guia de todos!" - guia criado por <span>Gabriel</span></p>
+                                </div>
+
+                                <div className="app__user-comment">
+                                    <p>Este guia é realmente muito bom!</p>
+                                </div>
+
+                                
+                            </div>  
+
+                            <div className="app__cms-c">
+                                <div className="app__user-pic">
+                                    <img src={selectedImage} alt="profile" />
+                                </div>
+
+                                <div className="app__user-name">
+                                    <span>{currentUser.username}</span>
+                                </div>
+
+                                <button>Apagar</button>
+
+                                <div className="app__guide-name">
+                                    <p>Aatrox Top - "O melhor guia de todos!" - guia criado por <span>Gabriel</span></p>
+                                </div>
+
+                                <div className="app__user-comment">
+                                    <p>Este guia é realmente muito bom!</p>
+                                </div>
+
+                                
+                            </div>  
+
+                            <div className="app__cms-c">
+                                <div className="app__user-pic">
+                                    <img src={selectedImage} alt="profile" />
+                                </div>
+
+                                <div className="app__user-name">
+                                    <span>{currentUser.username}</span>
+                                </div>
+
+                                <button>Apagar</button>
+
+                                <div className="app__guide-name">
+                                    <p>Aatrox Top - "O melhor guia de todos!" - guia criado por <span>Gabriel</span></p>
+                                </div>
+
+                                <div className="app__user-comment">
+                                    <p>Este guia é realmente muito bom!</p>
+                                </div>
+
+                                
+                            </div>  
+
+                            <div className="app__cms-c">
+                                <div className="app__user-pic">
+                                    <img src={selectedImage} alt="profile" />
+                                </div>
+
+                                <div className="app__user-name">
+                                    <span>{currentUser.username}</span>
+                                </div>
+
+                                <button>Apagar</button>
+
+                                <div className="app__guide-name">
+                                    <p>Aatrox Top - "O melhor guia de todos!" - guia criado por <span>Gabriel</span></p>
+                                </div>
+
+                                <div className="app__user-comment">
+                                    <p>Este guia é realmente muito bom!</p>
+                                </div>
+
+                                
+                            </div>  
+
+                            <div className="app__cms-c">
+                                <div className="app__user-pic">
+                                    <img src={selectedImage} alt="profile" />
+                                </div>
+
+                                <div className="app__user-name">
+                                    <span>{currentUser.username}</span>
+                                </div>
+
+                                <button>Apagar</button>
+
+                                <div className="app__guide-name">
+                                    <p>Aatrox Top - "O melhor guia de todos!" - guia criado por <span>Gabriel</span></p>
+                                </div>
+
+                                <div className="app__user-comment">
+                                    <p>Este guia é realmente muito bom!</p>
+                                </div>
+
+                                
+                            </div>  
+
+                            <div className="app__cms-c">
+                                <div className="app__user-pic">
+                                    <img src={selectedImage} alt="profile" />
+                                </div>
+
+                                <div className="app__user-name">
+                                    <span>{currentUser.username}</span>
+                                </div>
+
+                                <button>Apagar</button>
+
+                                <div className="app__guide-name">
+                                    <p>Aatrox Top - "O melhor guia de todos!" - guia criado por <span>Gabriel</span></p>
+                                </div>
+
+                                <div className="app__user-comment">
+                                    <p>Este guia é realmente muito bom!</p>
+                                </div>
+
+                                
+                            </div>                                                      
+                        </div>
+                    </div>
+                </div>               
         </section>
     )
 }
