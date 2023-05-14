edca27ff1a31f4a87cafb012dbac4fe13d443d82
Gabriel <gabrielsilvalucio@gmail.com>
2023-05-14 21:49:56 +0100

    ChampionAatrox JSX + SCSS Criação

---
+++ src/Pages/ChampionAatrox/ChampionAatrox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './ChampionAatrox.css';
+
+const ChampionAatrox = () => {
+
+    return (
+        <section>
+            
+        </section>
+    );
+
+}
+
+export default ChampionAatrox;
